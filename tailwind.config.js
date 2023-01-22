434fff6261aee0313eb5cbf0e58d03d5dd266d5a
Bekforever13 <b3kforever@gmail.com>
2023-01-22 17:03:32 +0500

    exam task completed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./*.html'],
+	theme: {
+		screens: {
+			lg: '1024px',
+			md: '768px',
+			sm: '500px',
+			xs: '0px',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
